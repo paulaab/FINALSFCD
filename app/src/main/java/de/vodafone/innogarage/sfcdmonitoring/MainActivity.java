51cee6a7249540453c46c75ba9f471ad538033a6
Paula Bohorquez <paulabohorquez@Paula.local>
2017-06-08 16:21:07 +0200

    Missing map

--- app/src/main/java/de/vodafone/innogarage/sfcdmonitoring/MainActivity.java
+++ app/src/main/java/de/vodafone/innogarage/sfcdmonitoring/MainActivity.java
@@ -92,41 +92,56 @@ public class MainActivity extends AppCompatActivity {
                                     case "gstatus":
                                         try {
                                             JSONObject gobj = result.getJSONObject("gstatus");
-                                            temp = (TextView) findViewById(R.id.ltebw);
-                                            temp.setText(gobj.getString("ltebw(mhz)"));
-                                            temp = (TextView) findViewById(R.id.pccrxdrssi);
-                                            temp.setText(gobj.getString("rsrp(dbm)pccrxdrssi"));
-                                            temp = (TextView) findViewById(R.id.pccrxrmrssi);
-                                            temp.setText(gobj.getString("rsrp(dbm)pccrxmrssi"));
-                                            temp = (TextView) findViewById(R.id.grsrq);
-                                            temp.setText(gobj.getString("rsrq(db)"));
-                                            temp = (TextView) findViewById(R.id.gsinr);
-                                            temp.setText(gobj.getString("sinr(db)"));
-                                            temp = (TextView) findViewById(R.id.gmode);
-                                            temp.setText(gobj.getString("mode"));
-                                            temp = (TextView) findViewById(R.id.gmode);
-                                            temp.setText(gobj.getString("mode"));
-                                            temp = (TextView) findViewById(R.id.gmode);
-                                            temp.setText(gobj.getString("mode"));
-                                            temp = (TextView) findViewById(R.id.gmode);
-                                            temp.setText(gobj.getString("mode"));
-                                            temp = (TextView) findViewById(R.id.gmode);
-                                            temp.setText(gobj.getString("mode"));
-                                            temp = (TextView) findViewById(R.id.gmode);
-                                            temp.setText(gobj.getString("mode"));
-                                            temp = (TextView) findViewById(R.id.gmode);
-                                            temp.setText(gobj.getString("mode"));
-                                            temp = (TextView) findViewById(R.id.gmode);
-                                            temp.setText(gobj.getString("mode"));
-                                            temp = (TextView) findViewById(R.id.gmode);
-                                            temp.setText(gobj.getString("mode"));
-                                            temp = (TextView) findViewById(R.id.gmode);
-                                            temp.setText(gobj.getString("mode"));
-                                            temp = (TextView) findViewById(R.id.gmode);
-                                            temp.setText(gobj.getString("mode"));
-
-
-
+                                            if(gobj!=null){
+                                                temp = (TextView) findViewById(R.id.ltebw);
+                                                temp.setText(gobj.getString("ltebw(mhz)"));
+                                                temp = (TextView) findViewById(R.id.rsrprxdr);
+                                                temp.setText(gobj.getString("rsrp(dbm)pccrxdrssi"));
+                                                temp = (TextView) findViewById(R.id.rsrprxm);
+                                                temp.setText(gobj.getString("rsrp(dbm)pccrxmrssi"));
+                                                temp = (TextView) findViewById(R.id.grsrq);
+                                                temp.setText(gobj.getString("rsrq(db)"));
+                                                temp = (TextView) findViewById(R.id.gsinr);
+                                                temp.setText(gobj.getString("sinr(db)"));
+                                                temp = (TextView) findViewById(R.id.gmode);
+                                                temp.setText(gobj.getString("mode"));
+                                                temp = (TextView) findViewById(R.id.ltecastate);
+                                                temp.setText(gobj.getString("ltecastate"));
+                                                temp = (TextView) findViewById(R.id.cellid);
+                                                temp.setText(gobj.getString("cellid"));
+                                                temp = (TextView) findViewById(R.id.currenttime);
+                                                temp.setText(gobj.getString("currenttime"));
+                                                temp = (TextView) findViewById(R.id.ltetxchan);
+                                                temp.setText(gobj.getString("ltetxchan"));
+                                                temp = (TextView) findViewById(R.id.gtac);
+                                                temp.setText(gobj.getString("tac"));
+                                                temp = (TextView) findViewById(R.id.emmstatereg);
+                                                temp.setText(gobj.getString("emmstatereg"));
+                                                temp = (TextView) findViewById(R.id.rrcstate);
+                                                temp.setText(gobj.getString("rrcstate"));
+                                                temp = (TextView) findViewById(R.id.temperature);
+                                                temp.setText(gobj.getString("temperature"));
+                                                temp = (TextView) findViewById(R.id.systemmode);
+                                                temp.setText(gobj.getString("systemmode"));
+                                                temp = (TextView) findViewById(R.id.psstate);
+                                                temp.setText(gobj.getString("psstate"));
+                                                temp = (TextView) findViewById(R.id.emmstateserv);
+                                                temp.setText(gobj.getString("emmstateserv"));
+                                                temp = (TextView) findViewById(R.id.lteband);
+                                                temp.setText(gobj.getString("lteband"));
+                                                temp = (TextView) findViewById(R.id.lterxchan);
+                                                temp.setText(gobj.getString("lterxchan"));
+                                                temp = (TextView) findViewById(R.id.gtxpower);
+                                                temp.setText(gobj.getString("txpower"));
+                                                temp = (TextView) findViewById(R.id.imsregstate);
+                                                temp.setText(gobj.getString("imsregstate"));
+                                                temp = (TextView) findViewById(R.id.resetcounter);
+                                                temp.setText(gobj.getString("resetcounter"));
+                                                temp = (TextView) findViewById(R.id.pccrxdrssi);
+                                                temp.setText(gobj.getString("pccrxdrssi"));
+                                                temp = (TextView) findViewById(R.id.pccrxrmrssi);
+                                                temp.setText(gobj.getString("pccrxmrssi"));
+                                            }
 
                                         } catch (JSONException e) {
                                             System.out.println("Could not extract gstatus object -->");
@@ -134,7 +149,211 @@ public class MainActivity extends AppCompatActivity {
 
                                         }
                                         break;
+
+
+                                    case "serving":
+                                        try {
+                                            JSONArray sarr = result.getJSONArray("serving");
+                                            if (sarr != null) {
+                                                JSONObject sobj = sarr.getJSONObject(0);
+
+
+                                                temp = (TextView) findViewById(R.id.searfcn);
+                                                temp.setText(sobj.getString("EARFCN"));
+
+                                                temp = (TextView) findViewById(R.id.smcc);
+                                                temp.setText(sobj.getString("MCC"));
+
+                                                temp = (TextView) findViewById(R.id.smnc);
+                                                temp.setText(sobj.getString("MNC"));
+
+                                                temp = (TextView) findViewById(R.id.stac);
+                                                temp.setText(sobj.getString("TAC"));
+
+                                                temp = (TextView) findViewById(R.id.scid);
+                                                temp.setText(sobj.getString("CID"));
+
+                                                temp = (TextView) findViewById(R.id.sbd);
+                                                temp.setText(sobj.getString("Bd"));
+
+                                                temp = (TextView) findViewById(R.id.sd);
+                                                temp.setText(sobj.getString("D"));
+
+                                                temp = (TextView) findViewById(R.id.su);
+                                                temp.setText(sobj.getString("U"));
+
+                                                temp = (TextView) findViewById(R.id.ssnr);
+                                                temp.setText(sobj.getString("SNR"));
+
+                                                temp = (TextView) findViewById(R.id.spci);
+                                                temp.setText(sobj.getString("PCI"));
+
+                                                temp = (TextView) findViewById(R.id.srsrq);
+                                                temp.setText(sobj.getString("RSRQ"));
+
+                                                temp = (TextView) findViewById(R.id.srsrp);
+                                                temp.setText(sobj.getString("RSRP"));
+
+                                                temp = (TextView) findViewById(R.id.srssi);
+                                                temp.setText(sobj.getString("RSSI"));
+
+                                                temp = (TextView) findViewById(R.id.srxlv);
+                                                temp.setText(sobj.getString("RXLV"));
+                                            }
+
+                                        } catch (JSONException e) {
+                                            System.out.println("Could not extract serving object -->");
+                                            e.printStackTrace();
+                                        }
+                                        break;
+
+
+                                    case "interfreq":
+                                        try {
+                                            JSONArray itearr = result.getJSONArray("interfreq");
+                                            if(itearr!=null){
+                                                JSONObject iteobj= itearr.getJSONObject(0);
+
+                                                    temp = (TextView) findViewById(R.id.iteearfcn);
+                                                    temp.setText(iteobj.getString("EARFCN"));
+
+                                                    temp = (TextView) findViewById(R.id.thresholdlow);
+                                                    temp.setText(iteobj.getString("ThresholdLow"));
+
+                                                    temp = (TextView) findViewById(R.id.thresholdhi);
+                                                    temp.setText(iteobj.getString("ThresholdHi"));
+
+                                                    temp = (TextView) findViewById(R.id.priority);
+                                                    temp.setText(iteobj.getString("Priority"));
+
+                                                    temp = (TextView) findViewById(R.id.itepci);
+                                                    temp.setText(iteobj.getString("PCI"));
+
+                                                    temp = (TextView) findViewById(R.id.itersrq);
+                                                    temp.setText(iteobj.getString("RSRQ"));
+
+                                                    temp = (TextView) findViewById(R.id.itersrp);
+                                                    temp.setText(iteobj.getString("RSRP"));
+
+                                                    temp = (TextView) findViewById(R.id.iterssi);
+                                                    temp.setText(iteobj.getString("RSSI"));
+
+                                                    temp = (TextView) findViewById(R.id.iterxlv);
+                                                    temp.setText(iteobj.getString("RXLV"));
+
+
+                                            }
+
+                                        }catch (JSONException e){
+                                            System.out.println("Could not extract interfreq object -->");
+                                            e.printStackTrace();
+                                        }
+                                        break;
+                                    case "intrafreq":
+                                        try {
+                                            JSONArray itaarr = result.getJSONArray("intrafreq");
+                                            if(itaarr!=null){
+                                                JSONObject itaobj1= itaarr.getJSONObject(0);
+                                                JSONObject itaobj2= itaarr.getJSONObject(1);
+
+
+                                                    temp = (TextView) findViewById(R.id.itapci1);
+                                                    temp.setText(itaobj1.getString("PCI"));
+
+                                                    temp = (TextView) findViewById(R.id.itarsrq1);
+                                                    temp.setText(itaobj1.getString("RSRQ"));
+
+                                                    temp = (TextView) findViewById(R.id.itarsrp1);
+                                                    temp.setText(itaobj1.getString("RSRP"));
+
+                                                    temp = (TextView) findViewById(R.id.itarssi1);
+                                                    temp.setText(itaobj1.getString("RSSI"));
+
+                                                    temp = (TextView) findViewById(R.id.itarxlv1);
+                                                    temp.setText(itaobj1.getString("RXLV"));
+
+                                                    temp = (TextView) findViewById(R.id.itapci2);
+                                                    temp.setText(itaobj2.getString("PCI"));
+
+                                                    temp = (TextView) findViewById(R.id.itarsrq2);
+                                                    temp.setText(itaobj2.getString("RSRQ"));
+
+                                                    temp = (TextView) findViewById(R.id.itarsrp2);
+                                                    temp.setText(itaobj2.getString("RSRP"));
+
+                                                    temp = (TextView) findViewById(R.id.itarssi2);
+                                                    temp.setText(itaobj2.getString("RSSI"));
+
+                                                    temp = (TextView) findViewById(R.id.itarxlv2);
+                                                    temp.setText(itaobj2.getString("RXLV"));
+
+
+
+                                            }
+
+                                        }catch (JSONException e){
+                                            System.out.println("Could not extract intrafreq object -->");
+                                            e.printStackTrace();
+                                        }
+                                        break;
+
+                                    case "location":
+                                        try{
+                                            JSONObject lobj = result.getJSONObject("location");
+                                            if (lobj != null){
+
+                                                temp = (TextView) findViewById(R.id.altitude);
+                                                temp.setText(lobj.getString("altitude"));
+                                                temp = (TextView) findViewById(R.id.ept);
+                                                temp.setText(lobj.getString("ept"));
+                                                temp = (TextView) findViewById(R.id.climb);
+                                                temp.setText(lobj.getString("climb"));
+                                                temp = (TextView) findViewById(R.id.eps);
+                                                temp.setText(lobj.getString("eps"));
+                                                temp = (TextView) findViewById(R.id.epv);
+                                                temp.setText(lobj.getString("epv"));
+                                                temp = (TextView) findViewById(R.id.epx);
+                                                temp.setText(lobj.getString("epx"));
+                                                temp = (TextView) findViewById(R.id.speed);
+                                                temp.setText(lobj.getString("speed"));
+                                                temp = (TextView) findViewById(R.id.track);
+                                                temp.setText(lobj.getString("track"));
+                                                temp = (TextView) findViewById(R.id.longitude);
+                                                temp.setText(lobj.getString("longitude"));
+                                                temp = (TextView) findViewById(R.id.latitude);
+                                                temp.setText(lobj.getString("latitude"));
+                                                temp = (TextView) findViewById(R.id.satellites);
+                                                temp.setText(lobj.getString("satellites"));
+                                                temp = (TextView) findViewById(R.id.mode);
+                                                temp.setText(lobj.getString("mode"));
+
+
+                                                }
+                                            }
+
+                                        catch (JSONException e){
+                                            System.out.println("Could not extract location object -->");
+                                            e.printStackTrace();
+                                            break;
+                                        }
+                                        break;
+
+
+
+
+
+
+
+
                                 }
+
+
+
+
+
+
+
+
                             }
 
 
